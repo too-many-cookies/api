e84b3875a3e987f8456a7877a373ca42e866210c
Jordan Rabideau <jmr8990@g.rit.edu>
2022-08-27 15:41:23 -0400

    create express app

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const api = require('./api')
+const express = require("express");
+const api = require("./api");
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
 
-app.use('/api', api)
+app.use("/api", api);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
+  console.log(`Server listening on ${PORT}`);
+});
