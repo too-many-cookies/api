627389cbdb85c521f6a19c274bc77726ae301bbb
Jordan Rabideau <jmr8990@g.rit.edu>
2022-10-07 14:39:22 -0400

    add more routes/controllers

--- api/controllers/analyticsController.js
+++ api/controllers/analyticsController.js
@@ -1,17 +1,33 @@
 const conn = require("../models/db.js");
 
 exports.test_api = (req, res) => {
-    res.send({message: "Controller test passed"})
-}
+  res.send({ message: "Controller test passed" });
+};
 
 exports.health_check = (req, res) => {
-    res.send({message: "Not implemented"})
-}
+  res.send({ message: "Not implemented" });
+};
 
 exports.get_classes = (req, res) => {
-    res.send({message: "Not implemented"})
-}
+  res.send({ message: "Not implemented" });
+};
 
 exports.get_class = (req, res) => {
-    res.send({message: "Not implemented"})
-}
+  res.send({ message: "Not implemented" });
+};
+
+exports.get_successful_logins = (req, res) => {
+  res.send({ message: "Not implemented" });
+};
+
+exports.get_failed_logins = (req, res) => {
+  res.send({ message: "Not implemented" });
+};
+
+exports.get_recent_logs = (req, res) => {
+  res.send({ message: "Not implemented" });
+};
+
+exports.get_students_by_class = (req, res) => {
+  res.send({ message: "Not implemented" });
+};
--- api/routes/analyticsRouter.js
+++ api/routes/analyticsRouter.js
@@ -1,11 +1,18 @@
-module.exports = app => {
-    const router = require("express").Router();
-    const analytics_controller = require("../controllers/analyticsController");
-    
-    router.get("/test", analytics_controller.test_api);
-    router.get("/healthz", analytics_controller.health_check);
-    router.get("/classes", analytics_controller.get_classes);
-    router.get("/classes/:id", analytics_controller.get_class);
-    
-    app.use('/v1', router)
-}
+module.exports = (app) => {
+  const router = require("express").Router();
+  const analytics_controller = require("../controllers/analyticsController");
+
+  router.get("/test", analytics_controller.test_api);
+  router.get("/healthz", analytics_controller.health_check);
+
+  router.get("/logins/success", analytics_controller.get_successful_logins);
+  router.get("/logins/failure", analytics_controller.get_failed_logins);
+  router.get("/recentLogs", analytics_controller.get_recent_logs);
+
+  router.get("/classes", analytics_controller.get_classes);
+  router.get("/classes/:id", analytics_controller.get_class);
+
+  router.get("/students/:classID", analytics_controller.get_students_by_class);
+
+  app.use("/v1", router);
+};
