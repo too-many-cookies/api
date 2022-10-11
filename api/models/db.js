93c24dffb7cdfa3a24401621ec4d3ec4b63e9fc3
Jordan <jordanmrabideau@gmail.com>
2022-10-11 13:53:57 -0400

    add user env variable

--- api/models/db.js
+++ api/models/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.STATUS === 'development' ? 'localhost' : process.env.HOST,
-    user: 'root',
+    user: process.env.STATUS === 'development' ? 'root' : process.env.USER,
     password: process.env.SQL_PASS,
     database: process.env.DB_NAME
 });
