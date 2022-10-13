b298c6380c7cd1d6700a1620139c15bdf650c416
Jordan Rabideau <jmr8990@g.rit.edu>
2022-10-13 13:43:44 -0400

    change requests to posts to allow for request bodies

--- api/routes/analyticsRouter.js
+++ api/routes/analyticsRouter.js
@@ -2,16 +2,15 @@ module.exports = (app) => {
   const router = require("express").Router();
   const analytics_controller = require("../controllers/analyticsController");
 
-  router.get("/test", analytics_controller.test_api);
   router.get("/healthz", analytics_controller.health_check);
 
-  router.get("/logins", analytics_controller.get_logins);
-  router.get("/logins/:classID", analytics_controller.get_logins_by_class)
+  router.post("/logins", analytics_controller.get_logins);
+  router.post("/logins/:classID", analytics_controller.get_logins_by_class);
 
-  router.get("/classes", analytics_controller.get_classes);
-  router.get("/classes/:classID", analytics_controller.get_class);
+  router.post("/classes", analytics_controller.get_classes);
+  router.post("/classes/:classID", analytics_controller.get_class);
 
-  router.get("/students/:classID", analytics_controller.get_students_by_class);
+  router.post("/students/:classID", analytics_controller.get_students_by_class);
 
   app.use("/v1", router);
 };
