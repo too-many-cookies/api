9050198039a15c3b9f9d5be56abf85f247b9cad6
Ethan Ruszanowski <ethan@rusz.dev>
2022-10-21 11:46:23 -0400

    Add db ping every 15 minutes

--- api/models/db.js
+++ api/models/db.js
@@ -2,13 +2,19 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.STATUS === "development" ? "localhost" : process.env.HOST,
-  user: process.env.STATUS === "development" ? "root" : process.env.USER,
-  password: process.env.SQL_PASS,
-  database: process.env.DB_NAME,
-  waitForConnection: true,
-  connectionLimit: 100,
-  queueLimit: 0,
+    host: process.env.STATUS === "development" ? "localhost" : process.env.HOST,
+    user: process.env.STATUS === "development" ? "root" : process.env.USER,
+    password: process.env.SQL_PASS,
+    database: process.env.DB_NAME,
+    waitForConnection: true,
+    connectionLimit: 100,
+    queueLimit: 0,
 });
+
+function keepAlive() {
+    pool.query('select 1')
+}
+
+setInterval(keepAlive, 900000) // Ping the database every 15 minutes
 
 module.exports = pool;
